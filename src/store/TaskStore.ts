bc189fbb5ec04394b98bc2ef7078686da73b703a
brandaoplaster <brandaoplaster@gmail.com>
2024-09-15 20:36:16 -0300

    feat: Added getters and typescript

--- src/store/TaskStore.ts
+++ src/store/TaskStore.ts
@@ -1,11 +1,28 @@
 import { defineStore } from "pinia";
 
+interface Task {
+  id: number;
+  title: string;
+  isFav: boolean;
+}
+
+interface TaskState {
+  tasks: Task[];
+}
+
 export const useTaskStore = defineStore('taskStore', {
-  state: () => ({
+  state: (): TaskState => ({
     tasks: [
-      {id: 1, title: "buy some milk", isFav: false},
-      {id: 2, title: "play Fifa", isFav: true},
-      {id: 3, title: "working in project", isFav: false},
+      { id: 1, title: "buy some milk", isFav: false },
+      { id: 2, title: "play Fifa", isFav: true },
+      { id: 3, title: "working in project", isFav: false },
     ]
-  })
-})
+  }),
+  getters: {
+    favs: (state): Task[] => state.tasks.filter(task => task.isFav),
+    favCount: (state): number => state.tasks.reduce((p, c) => {
+      return c.isFav ? p + 1 : p
+    }, 0),
+    totalCount: (state): number => state.tasks.length
+  },
+});
