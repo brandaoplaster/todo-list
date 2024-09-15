3416cf84986a17f41152a55e4153278e0d544208
brandaoplaster <brandaoplaster@gmail.com>
2024-09-14 21:19:15 -0300

    feat: Added useTaskStore and TaskDetails component

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,30 @@
       <img src="./assets/logo.png" alt=" pinia logo">
       <h1>Pinia Tasks</h1>
     </header>
+
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks" v-bind:key="task.id">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+
   </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { useTaskStore } from "./store/TaskStore";
+import TaskDetails from "./components/TaskDetails.vue";
 
 export default defineComponent({
   name: 'App',
   components: {
+    TaskDetails
+  },
+  setup() {
+    const taskStore = useTaskStore()
+
+    return { taskStore }
   }
 });
 </script>
