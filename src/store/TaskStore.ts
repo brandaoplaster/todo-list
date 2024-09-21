f4b7a39568876ea8c3112eec632fbc6f453710d5
brandaoplaster <brandaoplaster@gmail.com>
2024-09-20 22:27:09 -0300

    feat: Removing state

--- src/store/TaskStore.ts
+++ src/store/TaskStore.ts
@@ -13,11 +13,7 @@ interface TaskState {
 
 export const useTaskStore = defineStore('taskStore', {
   state: (): TaskState => ({
-    tasks: [
-      { id: 1, title: "buy some milk", isFav: false },
-      { id: 2, title: "play Fifa", isFav: true },
-      { id: 3, title: "working in project", isFav: false },
-    ],
+    tasks: [],
     isLoading: false
   }),
   getters: {
